283c66923ae778e78203d0d95243e01cfd2cd635
Henrique Rodrigues <henrique.grdr@inf.ufpel.edu.br>
2024-05-04 00:13:55 -0300

    Refactor and bug fix in the Direction Button

--- scripts/direction_button.gd
+++ scripts/direction_button.gd
@@ -1,12 +1,11 @@
-extends TextureButton
+extends Control
 
 @export var button_direction: PathProcessor.MOVES = PathProcessor.MOVES.RIGHT
 
 func _ready():
 	self.pivot_offset = Vector2(self.size.x / 2, self.size.y / 2)
+	%DirectionButton.pivot_offset = Vector2(self.size.x / 2, self.size.y / 2)
 
 func _on_pressed():
-	self.rotation_degrees += 90
+	%DirectionButton.rotation_degrees += 90
 	self.button_direction = (self.button_direction + 1) % PathProcessor.MOVES.size() as PathProcessor.MOVES
-	# Used for debugging
-	# PathProcessor.print_move(self.button_direction)
