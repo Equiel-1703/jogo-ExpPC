2a1e268366bc6c9ee02e81ade6f9b61cb7054663
Henrique Rodrigues <henrique.grdr@inf.ufpel.edu.br>
2024-05-05 18:22:10 -0300

    Fix Rocket formatting

--- scripts/level_1/rocket.gd
+++ scripts/level_1/rocket.gd
@@ -4,52 +4,52 @@ signal move_completed
 
 const MOVE_SPEED = 1.0
 
-var tile_size = GlobalGameData.MAP_TILE_SIZE.x
-var commands_to_process:Array
-var execute_flag:bool
+var _tile_size = GlobalGameData.MAP_TILE_SIZE.x
+var _commands_to_process: Array
+var _execute_flag: bool
 
 func _ready():
 	self.visible = false
-	execute_flag = false
+	_execute_flag = false
 
-func set_start_position(start_pos:Vector2) -> void:
+func set_start_position(start_pos: Vector2) -> void:
 	self.position = start_pos
 	self.visible = true
 
-var destination_pos:Vector2
-var t:float = 0.0
+var destination_pos: Vector2
+var t: float = 0.0
 
-func execute_move_commands(commands:Array) -> void:
-	commands_to_process = commands
+func execute_move_commands(commands: Array) -> void:
+	_commands_to_process = commands
 	execute_next_command()
 
 func execute_next_command() -> void:
-	if commands_to_process.size() > 0:
-		var command = commands_to_process.pop_front()
+	if _commands_to_process.size() > 0:
+		var command = _commands_to_process.pop_front()
 		destination_pos = self.position
 
 		match command:
 				PathProcessor.MOVES.UP:
-					destination_pos += Vector2(0, -tile_size)
+					destination_pos += Vector2(0, -_tile_size)
 					self.rotation_degrees = 0
 				PathProcessor.MOVES.DOWN:
-					destination_pos += Vector2(0, tile_size)
+					destination_pos += Vector2(0, _tile_size)
 					self.rotation_degrees = 180
 				PathProcessor.MOVES.LEFT:
-					destination_pos += Vector2(-tile_size, 0)
+					destination_pos += Vector2( - _tile_size, 0)
 					self.rotation_degrees = 270
 				PathProcessor.MOVES.RIGHT:
-					destination_pos += Vector2(tile_size, 0)
+					destination_pos += Vector2(_tile_size, 0)
 					self.rotation_degrees = 90
 		
 		t = 0.0
-		execute_flag = true
+		_execute_flag = true
 			
 	else:
 		move_completed.emit()
 
 func _physics_process(delta):
-	if execute_flag:
+	if _execute_flag:
 		if t < 0.5:
 			t += MOVE_SPEED * delta
 		
@@ -58,5 +58,5 @@ func _physics_process(delta):
 			# print(t)
 			# print(self.position)
 		else:
-			execute_flag = false
+			_execute_flag = false
 			execute_next_command()
