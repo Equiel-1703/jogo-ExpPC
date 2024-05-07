f5579b385730c476d59a59a0f10abb2a358bffcc
Henrique Rodrigues <henrique.grdr@inf.ufpel.edu.br>
2024-05-06 22:55:51 -0300

    Updated main screen image and changed scene folder structure

--- scripts/main_screen.gd
+++ scripts/main_screen.gd
@@ -1,7 +1,7 @@
 extends Control
 
 func _on_jogar_pressed():
-	get_tree().change_scene_to_file("res://levels/level_1.tscn")
+	get_tree().change_scene_to_file("res://scenes/levels/level_1.tscn")
 
 
 func _on_sair_pressed():
