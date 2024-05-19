392dad007bbed0054da0ed6a65f3c7045c4369a7
Henrique Rodrigues <henrique.grdr@inf.ufpel.edu.br>
2024-05-18 21:58:10 -0300

    Improved visuals

--- scripts/level_1/rocket.gd
+++ scripts/level_1/rocket.gd
@@ -1,4 +1,4 @@
-extends Sprite2D
+extends Node2D
 
 signal move_completed(final_position: Vector2)
 
@@ -8,55 +8,62 @@ var _tile_size = GlobalGameData.MAP_TILE_SIZE.x
 var _commands_to_process: Array
 var _execute_flag: bool
 
+@onready var _propulsion = $Propulsion
+
 func _ready():
-	self.visible = false
 	_execute_flag = false
+	_propulsion.emitting = false
 
 func set_start_position(start_pos: Vector2) -> void:
 	self.position = start_pos
-	self.visible = true
 
-var destination_pos: Vector2
-var t: float = 0.0
+var _destination_pos: Vector2
+var _t: float = 0.0
 
 func execute_move_commands(commands: Array) -> void:
+	# Store the commands to process
 	_commands_to_process = commands
+	# Turn on the propulsion effect
+	_propulsion.emitting = true
+	# Execute the first command
 	_execute_next_command()
 
 func _execute_next_command() -> void:
 	if _commands_to_process.size() > 0:
 		var command = _commands_to_process.pop_front()
-		destination_pos = self.position
-
+		_destination_pos = self.position
+		
 		match command:
 				PathProcessor.MOVES.UP:
-					destination_pos += Vector2(0, -_tile_size)
+					_destination_pos += Vector2(0, -_tile_size)
 					self.rotation_degrees = 0
 				PathProcessor.MOVES.DOWN:
-					destination_pos += Vector2(0, _tile_size)
+					_destination_pos += Vector2(0, _tile_size)
 					self.rotation_degrees = 180
 				PathProcessor.MOVES.LEFT:
-					destination_pos += Vector2( - _tile_size, 0)
+					_destination_pos += Vector2( - _tile_size, 0)
 					self.rotation_degrees = 270
 				PathProcessor.MOVES.RIGHT:
-					destination_pos += Vector2(_tile_size, 0)
+					_destination_pos += Vector2(_tile_size, 0)
 					self.rotation_degrees = 90
 		
-		t = 0.0
+		_t = 0.0
 		_execute_flag = true
 			
 	else:
+		# Turn off the propulsion effect
+		_propulsion.emitting = false
 		# The last destination position is the final position of the rocket
-		move_completed.emit(destination_pos)
+		move_completed.emit(_destination_pos)
 
 func _physics_process(delta):
 	if _execute_flag:
-		if t < 0.5:
-			t += MOVE_SPEED * delta
+		if _t < 0.5:
+			_t += MOVE_SPEED * delta
 		
-			self.position = self.position.lerp(destination_pos, t)
+			self.position = self.position.lerp(_destination_pos, _t)
 			# Used for debug purposes only
-			# print(t)
+			# print(_t)
 			# print(self.position)
 		else:
 			_execute_flag = false
