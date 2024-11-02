79db312b4bf2b5bc97f9d9cb0cfc8a3ee8b84ea3
Henrique Rodrigues <henrique.grdr@inf.ufpel.edu.br>
2024-11-02 11:40:51 -0300

    feat: added buttons to student name screen

--- scripts/general/student_name.gd
+++ scripts/general/student_name.gd
@@ -7,3 +7,9 @@ func _on_tam_rota_line_field_text_submitted(new_text: String) -> void:
 
 	# Going to first level
 	get_tree().change_scene_to_file(GlobalGameData.start_level_scene_path)
+
+func _on_ok_pressed() -> void:
+	_on_tam_rota_line_field_text_submitted(%TamRotaLineField.text)
+
+func _on_cancelar_pressed() -> void:
+	get_tree().change_scene_to_packed(GlobalGameData.main_screen)
